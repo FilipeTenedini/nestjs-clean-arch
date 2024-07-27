55b9f34c2c2211256c46a7298af9163482c36256
FilipeTenedini <tenedinifilipe@gmail.com>
2024-07-27 11:15:39 -0300

    chore: create jest configs to test application

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import { pathsToModuleNameMapper } from "ts-jest";
+import { compilerOptions } from "./tsconfig.json";
+
+export default {
+    moduleFileExtensions: ["js", "json", "ts"],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: "<rootDir>/", // This is needed to match the paths in the tsconfig.json
+    }),
+    testRegex: ".*\\..*spec\\.ts$",
+    transform: {
+        "^.+\\.(t|j)s$": "ts-jest",
+    },
+    collectCoverageFrom: ["**/*.(t|j)s"],
+    coverageDirectory: "../coverage",
+    testEnvironment: "node",
+};
