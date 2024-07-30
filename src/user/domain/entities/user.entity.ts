cc240e4ca1d7576321ee4c6397b061e88f5a1e06
FilipeTenedini <tenedinifilipe@gmail.com>
2024-07-30 18:42:24 -0300

    feat: create user entity

--- src/user/domain/entities/user.entity.ts
+++ src/user/domain/entities/user.entity.ts
@@ -1,1 +1,30 @@
-export class UserEntity {}
+import { BaseEntity } from "@/shared/domain/entities/base-entity";
+
+export interface UserProps {
+    name: string;
+    email: string;
+    password: string;
+    createdAt?: Date;
+}
+
+export class UserEntity extends BaseEntity<UserProps> {
+    constructor(
+        public readonly props: UserProps,
+        id?: string,
+    ) {
+        super(props, id);
+        this.props.createdAt = this.props.createdAt ?? new Date();
+    }
+
+    get name() {
+        return this.props.name;
+    }
+
+    get email() {
+        return this.props.email;
+    }
+
+    get password() {
+        return this.props.password;
+    }
+}
