95bfee804756b1152b48d4455a95405bbf89efb5
FilipeTenedini <tenedinifilipe@gmail.com>
2024-08-18 15:48:30 -0300

    feat: create in memory user repository implementation

---
+++ src/user/infra/database/repositories/in-memory-user.repository.ts
@@ -0,0 +1,12 @@
+import { UserEntity, UserProps } from "@/user/domain/entities/user.entity";
+import { UserRepository } from "./user.repository";
+import { InMemoryRepository } from "@/shared/domain/repositories/in-memory.repository";
+
+export class InMemoryUserRepository
+    extends InMemoryRepository<UserProps, UserEntity>
+    implements UserRepository
+{
+    public async findByEmail(email: string): Promise<UserEntity | null> {
+        return this.data.find((user) => user.email === email);
+    }
+}
