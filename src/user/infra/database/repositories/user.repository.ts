ab2d8ac1ff47cb2e1f76dfacd30f978a0c48cd63
FilipeTenedini <tenedinifilipe@gmail.com>
2024-08-18 20:27:12 -0300

    refactor: pass UserProps through the UserRepository

--- src/user/infra/database/repositories/user.repository.ts
+++ src/user/infra/database/repositories/user.repository.ts
@@ -1,6 +1,6 @@
 import { RepositoryContract } from "@/shared/domain/repositories/repository-contract.repository";
-import { UserEntity } from "@/user/domain/entities/user.entity";
+import { UserEntity, UserProps } from "@/user/domain/entities/user.entity";
 
-export abstract class UserRepository extends RepositoryContract<UserEntity> {
+export abstract class UserRepository extends RepositoryContract<UserProps, UserEntity> {
     public abstract findByEmail(email: string): Promise<UserEntity | null>;
 }
