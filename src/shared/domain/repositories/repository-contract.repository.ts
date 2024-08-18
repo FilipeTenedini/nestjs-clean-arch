fdf57a6e81ccc8495e7f298e12218e7cc0caec2a
FilipeTenedini <tenedinifilipe@gmail.com>
2024-08-18 20:26:21 -0300

    feat: add params to findAll method on Repository contract

--- src/shared/domain/repositories/repository-contract.repository.ts
+++ src/shared/domain/repositories/repository-contract.repository.ts
@@ -1,7 +1,15 @@
-export abstract class RepositoryContract<E> {
+export type SearchProps<F> = {
+    page?: number;
+    limit?: number;
+    sort?: string | null;
+    sortOrder?: "asc" | "desc" | null;
+    filter?: F | null;
+};
+
+export abstract class RepositoryContract<P, E> {
     public abstract insert(entity: E): Promise<void>;
     public abstract update(entity: E): Promise<E>;
     public abstract delete(id: string): Promise<void>;
     public abstract findById(id: string): Promise<E | null>;
-    public abstract findAll(): Promise<E[]>;
+    public abstract findAll(params: SearchProps<P>): Promise<E[]>;
 }
