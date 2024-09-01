66fd0a44e98779eeddf0738e6f726913139f5096
FilipeTenedini <tenedinifilipe@gmail.com>
2024-09-01 18:48:34 -0300

    feat: create get user usecase

---
+++ src/user/application/usecases/get-user.usecase.ts
@@ -0,0 +1,17 @@
+import { UserRepository } from "@/user/infra/database/repositories/user.repository";
+import { UserProps } from "@/user/domain/entities/user.entity";
+import { NotFoundError } from "@/shared/errors/not-found-error";
+
+export class GetUserUseCase {
+    constructor(private userRepository: UserRepository) {}
+
+    async execute(id: string): Promise<UserProps> {
+        const user = await this.userRepository.findById(id);
+
+        if (!user) {
+            throw new NotFoundError("User not found");
+        }
+
+        return user.toPlain();
+    }
+}
