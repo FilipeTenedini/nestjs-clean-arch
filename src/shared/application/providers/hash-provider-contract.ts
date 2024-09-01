340bdfdc0c77b0aa3aabfb18a5fe6227ee5438e9
FilipeTenedini <tenedinifilipe@gmail.com>
2024-09-01 19:21:22 -0300

    feat: use abstract class on hash provider contract1

--- src/shared/application/providers/hash-provider-contract.ts
+++ src/shared/application/providers/hash-provider-contract.ts
@@ -1,4 +1,4 @@
-export interface HashProviderContract {
-    generateHash(value: string): Promise<string>;
-    compareHash(value: string, hash: string): Promise<boolean>;
+export abstract class HashProviderContract {
+    public abstract generateHash(value: string): Promise<string>;
+    public abstract compareHash(value: string, hash: string): Promise<boolean>;
 }
